84b53c94195b9ad820c14293184ab7ec61a4598a
Shaun Becker <Shaun.Becker@marcusmillichap.com>
2023-04-29 16:37:35 -0700

    Fix for bad var name

--- pages/Home.js
+++ pages/Home.js
@@ -45,7 +45,7 @@ function Home({ bannerText, galleryCardInfo }) {
     //const res = await fetch('http://localhost:1337/api/properties', { headers: { 'Authorization': 'Bearer 86d4fa545fb168b6b3077eac895f825968eaef438ef8dc0ae8cd1e5df7a56be8cca5e38b927de2a0d04004bb3694f353ebff07208c508f78becb4091816bec71d50e88a6d53eb916bef0751c3a7cd41249578ef3f626e3a8f12057c1c28a31dbdf4441309ed480b025d712452552798ed447d5f849c1a7d02f613ed029125353' } });
     //const jsonResponse = (await res.json());
 
-    const properties = properties.data.map(d => d.attributes);
+    const properties = jsonResponse.data.map(d => d.attributes);
 
     //Can get data from CMS and compile at build time
     return {
