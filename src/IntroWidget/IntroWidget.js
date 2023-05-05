113c86005c644ef2fd4f319c5200b37db906f7bc
Shaun Becker <Shaun.Becker@marcusmillichap.com>
2023-05-05 10:09:34 -0700

    Update for vimeo

--- src/IntroWidget/IntroWidget.js
+++ src/IntroWidget/IntroWidget.js
@@ -3,6 +3,11 @@ import styles  from './IntroWidget.module.css'
 function GlobalHeader({ bannerText }) {
     return (
         <div className={styles.widgetBackdrop}>
+            <div className={styles.vimeoOuterWrapper}>
+                <div className={styles.vimeoInnerWrapper}>
+                <iframe className={styles.vimeoVideo} id="vim-cf15651f-5ddb-4465-9cc3-50565fa3b299" data-video-id="427061317" class="embed-responsive-item" type="text/html" title="Vimeo Video" src="//player.vimeo.com/video/427061317?badge=0&amp;background=1" data-ready="true"></iframe>
+                </div>
+            </div>
             <div className={styles.introContentContainer}>
                 <div className={styles.locationSection}>
                     <p className={styles.locationText}>Location: Pacific Northwest | Change Location</p>
